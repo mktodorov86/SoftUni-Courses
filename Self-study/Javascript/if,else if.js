2bb195a2d0f1588cbd9940176b52cfe0b3050552
mktodorov86 <mktodorov86@gmail.com>
2023-10-30 20:50:39 +0200

    last

--- Self-study/Javascript/if,else if.js
+++
@@ -1,9 +0,0 @@
-hour = -1;
-if (hour >=6 && hour < 12)
-    console.log("Good morning");
-else if (hour > 12 && hour <18)
-    console.log("Good afternoon");
-else if (hour > 24 || hour < 0)
-    console.log("ERROR");
-else
-    console.log("Good evening");
